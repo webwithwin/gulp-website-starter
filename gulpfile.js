9bbf8b8317045990013c1d27720d4a086d4459c4
Winardi <winardidesign@yahoo.com>
2019-10-11 16:56:02 +0700

    added hello world

---
+++ gulpfile.js
@@ -0,0 +1,90 @@
+const gulp = require('gulp');
+const browsersync = require('browser-sync').create();
+const sass = require('gulp-sass');
+const cleanCSS = require('gulp-clean-css');
+const rename = require('gulp-rename');
+const del = require('del');
+
+const paths = {
+  layouts: {
+    src: 'app/**/*.html',
+    dest: 'build'
+  },
+  styles: {
+    src: 'app/scss/**/*.scss',
+    dest: 'build/css'
+  },
+  scripts: {
+    src: 'app/js/**/*.js',
+    dest: 'build/js'
+  }
+};
+
+// Open browser
+function browserSync(done) {
+  browsersync.init({
+    server: {
+      baseDir: './build/'
+    },
+    port: 8888
+  });
+  done();
+}
+
+// BrowserSync Reload
+function browserSyncReload(done) {
+  browsersync.reload();
+  done();
+}
+
+// Clean build files
+function clean() {
+  return del(['./build/']);
+}
+
+function html() {
+  return gulp
+    .src(paths.layouts.src)
+    .pipe(gulp.dest(paths.layouts.dest))
+    // return stream
+    .pipe(browsersync.stream())
+}
+
+function css() {
+  return gulp
+    .src(paths.styles.src)
+    .pipe(sass())
+    .pipe(cleanCSS())
+    .pipe(rename({
+      suffix: '.min'
+    }))
+    .pipe(gulp.dest(paths.styles.dest))
+    .pipe(browsersync.stream())
+}
+
+function js() {
+  return gulp
+    .src(paths.scripts.src)
+    .pipe(gulp.dest(paths.scripts.dest))
+    // return stream
+    .pipe(browsersync.stream())
+}
+
+function watchFiles() {
+  gulp.watch(paths.layouts.src, html)
+  gulp.watch(paths.styles.src, css)
+  gulp.watch(paths.scripts.src, js)
+}
+
+const watch = gulp.parallel(watchFiles, browserSync);
+const build = gulp.series(clean, gulp.parallel(html, css, js));
+
+exports.html = html;
+exports.css = css;
+exports.js = js;
+exports.clean = clean;
+exports.watch = watch;
+exports.build = build;
+
+// Default gulp
+exports.default = watch;
